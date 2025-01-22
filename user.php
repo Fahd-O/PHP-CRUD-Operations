33d3b735f66e21a047c909eca5cf93484285fb1a
Abiola Fahad Ogunnaike <realabiolafahad@gmail.com>
2025-01-22 23:31:18 +0100

    Adjusted indentation and added form elements for user input in user.php

--- user.php
+++ user.php
@@ -1,20 +1,47 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>PHP CRUD Operations</title>
-  </head>
-  <body>
-    <div class="container">
-        
-    </div>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
 
-    
-  </body>
+        <title>PHP CRUD Operations</title>
+    </head>
+
+    <body>
+        <div class="container my-5">
+
+            <form method="post">
+
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" class="form-control" placeholder="Enter your name" name="name" autocomplete="off">
+                </div>
+                
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" class="form-control" placeholder="Enter your email" name="email" autocomplete="off">
+                </div>
+
+                <div class="form-group">
+                    <label>Mobile number</label>
+                    <input type="text" class="form-control" placeholder="Enter your mobile number" name="mobileNumber" autocomplete="off">
+                </div>
+
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" class="form-control" placeholder="Enter your password" name="pwd" autocomplete="off">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Submit</button>
+
+            </form>
+        </div>
+
+    </body>
+
 </html>
