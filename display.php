c24ac70db624e845c3405a0c21d5de5dbaa3183c
Abiola Fahad Ogunnaike <realabiolafahad@gmail.com>
2025-01-25 22:48:54 +0100

    added new file: display.php. Database content will be displayed here

---
+++ display.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+
+    <title>PHP CRUD Operations</title>
+</head>
+<body>
+    
+    <div class="container">
+        <button class="btn btn-primary my-5"><a href="user.php" target="_blank" rel="noopener noreferrer" class="text-light">Add User</a></button>
+    </div>
+
+</body>
+</html>
