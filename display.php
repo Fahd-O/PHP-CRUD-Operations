ef02aba8e9e4285122e33c4ff7afd114402ab2c2
Abiola Fahad Ogunnaike <realabiolafahad@gmail.com>
2025-01-27 22:11:35 +0100

    added table structure to display.php for user data presentation

--- display.php
+++ display.php
@@ -1,19 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-     <!-- Bootstrap CSS -->
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
 
     <title>PHP CRUD Operations</title>
 </head>
+
 <body>
-    
+
     <div class="container">
-        <button class="btn btn-primary my-5"><a href="user.php" target="_blank" rel="noopener noreferrer" class="text-light">Add User</a></button>
+        <button class="btn btn-primary my-5">
+            <a href="user.php" target="_blank" rel="noopener noreferrer" class="text-light">Add User</a>
+        </button>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">S/N</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Mobile Number</th>
+                    <th scope="col">Password</th>           
+                    <th scope="col">Operations</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 
 </body>
+
 </html>
